649b46cee9dd71cc307b4db67aa9e9326b4c0a00
Nitin Kumar <nitin2111kr@gmail.com>
2026-01-05 14:10:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// // // let par = document.getElementById('para');
+// // // console.log(par);
+
+// // // par.innerText='hello';
+// // // para.innerText='world'
+// // // console.log(par);
+// // // console.log(para);
+
+
+
+// // // let di = document.getElementsByClassName('a');
+// // // console.log(di);
+// // // di[0].innerText = "hello";
+// // // console.log(di);
+// // let para = document.getElementsByTagName("p");
+// // console.log(para);
+// // para[0].innerText = "hii";
+// // para[1].innerText = "Nitin";
+// // para[2].innerHTML = "<b>Nitin</b>"
+
+
+
+
+
+// console.log(a)
+
+
+// let para = document.querySelectorAll('.a');
+// console.log(para);
+// para[0].innerText = "hello";
+// para[1].innerText = "Nitin";
+// para[2].innerHTML = "<b>Nitin</b>"
+
+
+
+
+
+
+
+
+
